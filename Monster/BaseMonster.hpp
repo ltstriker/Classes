75b6b15bf3c0b88d2df386369d2d41cb27c456f7
leungyukshing <jacky14.liang@gmail.com>
2018-07-19 10:07:06 +0800

    add monster

---
+++ Monster/BaseMonster.hpp
@@ -0,0 +1,111 @@
+#pragma once
+#ifndef _BASE_MONSTER_H_
+#define _BASE_MONSTER_H_
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+typedef enum {
+  stateNone = 0, // 无状态
+  stateWalkRight, // 向右走
+  stateWalkLeft, // 向左走
+  stateWalkUp, // 向上走
+  stateWalkDown, // 向下走
+  stateAttackLeft, // 向左攻击
+  stateAttackRight, // 向右攻击
+  stateDeath,// 死亡
+  stateFrozen
+}MonsterState;
+
+typedef enum {
+  THUG = 0,
+  EXECUTIONER,
+  FALLEN,
+  IMMORTAL,
+  MUNRA,
+  RADIER,
+  SCORPION,
+  TREMOR,
+  WASPHORNET,
+  WASPQUEEN,
+  WOLF,
+  BOSS_EFREETI,
+  EFREETI,
+  CANIBAL,
+  GORILLA,
+  HUNTER,
+  PRIEST,
+  SHAMAN,
+  SHIELD,
+  WINGRIDER,
+  BOSS_CANIBAL,
+  CANIBAL_OFFSPRING,
+  BLAZEFANG,
+  BROODGUARD,
+  ELITE,
+  MYRMIDON,
+  NIGHTSCALE,
+  QUETZAL
+}MonsterName;
+
+class BaseMonster : public Sprite
+{
+public:
+  BaseMonster();
+  ~BaseMonster();
+  virtual bool init() override;
+  CREATE_FUNC(BaseMonster);
+  Sprite* baseSprite;
+  // 受伤
+  virtual void getHurt();
+  // 死亡
+  virtual void death();
+  // 爆炸
+  virtual void explosion();
+  CC_SYNTHESIZE(std::vector<Point>, pointsVector, PointsVector);
+  CC_SYNTHESIZE(bool, attackBySoldier, AttackBySoldier);
+  CC_SYNTHESIZE(MonsterName, monsterType, MonsterType);
+  CC_SYNTHESIZE(bool, attackByTower, AttackByTower);
+  CC_SYNTHESIZE(bool, isAttacking, IsAttacking);
+  CC_SYNTHESIZE(float, runSpeed, RunSpeed);
+  CC_SYNTHESIZE(float, maxHp, MaxHp);
+  CC_SYNTHESIZE(float, currHp, CurrHp);
+  CC_SYNTHESIZE(float, force, Force);
+  CC_SYNTHESIZE(float, armor, Armor);
+  CC_SYNTHESIZE(float, hpPercentage, HpPercentage);
+  CC_SYNTHESIZE(MonsterState, state, State);
+  CC_SYNTHESIZE(int, money, Money);
+  CC_SYNTHESIZE_READONLY(ProgressTimer*, hpBar, HpBar);
+  virtual void stopWalking();
+  virtual void restartWalking();
+  virtual void slowRunSpeed(float dt, float dis);
+  Point getNextPoint();
+  virtual void frozen();
+  virtual void refrozen(float dt);
+private:
+
+protected:
+  void setMonsterZorder(int yOrder);
+  int pointCounter;
+  Sprite* hpBgSprite;
+  Point currPoint();
+  Point nextPoint();
+  Point tempNextPoint;
+  void runNextPoint();
+  void createAndSetHpBar();
+
+  bool onTouchBegan(Touch* touch, Event* event);
+  void onTouchEnded(Touch* touch, Event* event);
+
+  virtual void update(float dt);
+  CC_SYNTHESIZE(bool, isSlowed, IsSlowed);
+
+  Sprite* blood;
+  MonsterState tempState;
+  MonsterState lastState;
+  void stopMonsterAnimation();
+  Sprite* ice;
+  void showInfo();
+  void addListener();
+};
+#endif
