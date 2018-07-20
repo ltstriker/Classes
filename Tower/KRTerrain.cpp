be047ce4628bb77c29da00ed6f930dfddb5aea58
Ltstriker <ltuse163@163.com>
2018-07-20 13:34:00 +0800

    debug tower v1.8
    1

--- Tower/KRTerrain.cpp
+++ Tower/KRTerrain.cpp
@@ -90,7 +90,7 @@ void KRTerrain::onTouchEnded(Touch * touch, Event * event)
 void KRTerrain::smokeEffect()
 {
 	//出售时的动画
-	auto smoke = Sprite::createWithSpriteFrameName("Smoke_01.png");
+	auto smoke = Sprite::createWithSpriteFrameName("effect_sellSmoke_0001.png");
 	addChild(smoke, 99);
 	smoke->runAction(Sequence::create(
 		Animate::create(AnimationCache::getInstance()->getAnimation("sell_smoke")),
